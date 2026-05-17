89b3d2008e40d3350f6a347bd43f7370907c1925
Matheus <matheusritzmann@gmail.com>
2026-05-17 18:19:33 -0300

    feat: cria estrutura inicial do projeto

---
+++ resources/views/inicio/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Página Inicial')
+
+@section('content')
+
+<div class="text-center">
+
+    <h1 class="display-4">
+        Sistema Inteligente para Corredores
+    </h1>
+
+    <p class="lead mt-4">
+        Plataforma para gerenciamento de métricas e sugestões de treinos personalizados.
+    </p>
+
+    <a href="#" class="btn btn-primary btn-lg mt-3">
+        Começar
+    </a>
+
+</div>
+
+@endsection
